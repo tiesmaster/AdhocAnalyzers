dc1055f28cf17f2f92fd573a6e8499d5833c847c
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-07-30 01:31:08 +0200

    Add refactoring for changing AutoFixture's
    	fixture.Create<string>()
    
    to
    
    	fixture.Build<string>().Create()

---
+++ AdhocRefactorings.Test/AutoFixture/CreateToBuildRefactoringProviderTests.cs
@@ -0,0 +1,96 @@
+﻿using AdhocRefactorings.AutoFixture;
+using AdhocRefactorings.Test.Helpers;
+
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+using Xunit;
+
+namespace AdhocRefactorings.Test.AutoFixture
+{
+    public class CreateToBuildRefactoringProviderTests : CodeRefactoringVerifier
+    {
+        [Fact]
+        public void CreateWithKeywordType()
+        {
+            var oldSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var fixture = new Fixture();
+        fixture.Create<string>();
+    }
+}";
+            var newSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var fixture = new Fixture();
+        fixture.Build<string>().Create();
+    }
+}";
+
+            VerifyRefactoring(oldSource, newSource, 100, "Convert Create<...>() to Build<...>().Create()");
+        }
+
+        [Fact]
+        public void CreateWithType()
+        {
+            var oldSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var fixture = new Fixture();
+        fixture.Create<Class1>();
+    }
+}";
+            var newSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var fixture = new Fixture();
+        fixture.Build<Class1>().Create();
+    }
+}";
+
+            VerifyRefactoring(oldSource, newSource, 100, "Convert Create<...>() to Build<...>().Create()");
+        }
+
+        [Fact]
+        public void CreateOnDifferentVariableName()
+        {
+            var oldSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var foo = new Fixture();
+        foo.Create<string>();
+    }
+}";
+            var newSource =
+@"class Class1
+{
+    void Method1()
+    {
+        var foo = new Fixture();
+        foo.Build<string>().Create();
+    }
+}";
+
+            VerifyRefactoring(oldSource, newSource, 92, "Convert Create<...>() to Build<...>().Create()");
+        }
+
+        // TODO: add support for providing the refactoring for the entire line of "fixture...., until the end of it
+
+        // TODO: add missing tests for scenario where we handle NOT providing the refactoring 
+
+        protected override CodeRefactoringProvider GetCodeRefactoringProvider()
+        {
+            return new CreateToBuildRefactoringProvider();
+        }
+    }
+}
---
+++ AdhocRefactorings/AdhocRefactorings/AutoFixture/CreateToBuildRefactoringProvider.cs
@@ -0,0 +1,80 @@
+﻿using System.Composition;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AdhocRefactorings.AutoFixture
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(CreateToBuildRefactoringProvider)), Shared]
+    public class CreateToBuildRefactoringProvider : CodeRefactoringProvider
+    {
+        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var currentNode = root.FindNode(context.Span);
+
+            var genericNode = currentNode as GenericNameSyntax;
+            if (genericNode == null || genericNode.Identifier.ValueText != "Create")
+            {
+                return;
+            }
+
+            var genericArguments = genericNode.TypeArgumentList.Arguments;
+            if (genericArguments.Count != 1)
+            {
+                return;
+            }
+
+            // TODO: needed? || fixtureCreateMemberAccessNode.IsKind(SyntaxKind.SimpleMemberAccessExpression))
+
+            var fixtureCreateMemberAccessNode = genericNode.Parent as MemberAccessExpressionSyntax;
+            if (fixtureCreateMemberAccessNode == null)
+            {
+                return;
+
+            }
+
+            var invocationNode = fixtureCreateMemberAccessNode.Parent as InvocationExpressionSyntax;
+            if (invocationNode == null)
+            {
+                return;
+            }
+
+            context.RegisterRefactoring(
+                CodeAction.Create(
+                    "Convert Create<...>() to Build<...>().Create()",
+                    _ =>
+                    {
+                        var fixtureBuildMemberAccessNode =
+                            SyntaxFactory.InvocationExpression(
+                                fixtureCreateMemberAccessNode.ReplaceToken(
+                                    genericNode.Identifier,
+                                    SyntaxFactory.Identifier("Build")));
+
+
+                        var newInvocationNode = SyntaxFactory.InvocationExpression(
+                            SyntaxFactory.MemberAccessExpression(
+                                SyntaxKind.SimpleMemberAccessExpression,
+                                fixtureBuildMemberAccessNode,
+                                SyntaxFactory.IdentifierName("Create")));
+
+                        var newRoot = root.ReplaceNode(invocationNode, newInvocationNode);
+                        return Task.FromResult(context.Document.WithSyntaxRoot(newRoot));
+                    }));
+        }
+
+        private Task<Document> ConvertCreateToBuild(Document document, SyntaxNode root, SyntaxNode nodeToReplace, PredefinedTypeSyntax typeSyntax)
+        {
+            var newNode = SyntaxFactory
+                .ParseExpression($"fixture.Build<{typeSyntax.Keyword.ValueText}>().Create()")
+                .WithLeadingTrivia(nodeToReplace.GetLeadingTrivia());
+            root = root.ReplaceNode(nodeToReplace, newNode);
+            return Task.FromResult(document.WithSyntaxRoot(root));
+        }
+    }
+}
