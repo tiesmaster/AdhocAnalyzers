0598669b57a0d129358ff4226ddaee4b71e0e646
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-09-03 11:16:47 +0200

    Add ToPrismProperty refactoring skeleton.

---
+++ AdhocAnalyzers/AdhocAnalyzers.Test/Prism/ToPrismPropertyTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdhocAnalyzers.Prism;
+using AdhocAnalyzers.Test.Helpers;
+
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Xunit;
+
+namespace AdhocAnalyzers.Test.Prism
+{
+    public class ToPrismPropertyTests : CodeRefactoringVerifier
+    {
+        [Fact]
+        public void EmptySource_ShouldNotProvideRefactoring()
+        {
+            var source = "";
+            VerifyNoRefactoring(source, 0);
+        }
+
+        protected override CodeRefactoringProvider GetCodeRefactoringProvider()
+            => new ToPrismPropertyRefactoringProvider();
+    }
+}
---
+++ AdhocAnalyzers/AdhocAnalyzers/Prism/ToPrismPropertyRefactoringProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Composition;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+namespace AdhocAnalyzers.Prism
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(StructureNamespaceUsingsRefactoringProvider))]
+    [Shared]
+    public class ToPrismPropertyRefactoringProvider : CodeRefactoringProvider
+    {
+        public override Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
