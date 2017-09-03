85fdd2d772c646e7be0f79e10de667c12727186b
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-09-03 11:24:39 +0200

    ToPrismProperty: Add some more scenarios where
    the refactoring should not be available.

--- AdhocAnalyzers/AdhocAnalyzers.Test/Prism/ToPrismPropertyTests.cs
+++ AdhocAnalyzers/AdhocAnalyzers.Test/Prism/ToPrismPropertyTests.cs
@@ -21,6 +21,44 @@ namespace AdhocAnalyzers.Test.Prism
             VerifyNoRefactoring(source, 0);
         }
 
+        [Fact]
+        public void EmptyClass_ShouldNotProvideRefactoring()
+        {
+            var source =
+@"class Class1
+{
+}";
+
+            VerifyNoRefactoring(source, 0);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(52)]
+        [InlineData(221)]
+        public void ClassWithPrismProperty_ShouldNotProvideRefactoring(int position)
+        {
+            var source =
+@"class Class1
+{
+    private int _property1;
+
+    public int Property1
+    {
+        get
+        {
+            return _property1;
+        }
+        set
+        {
+            _property1 = value;
+        }
+    }
+}";
+
+            VerifyNoRefactoring(source, position);
+        }
+
         protected override CodeRefactoringProvider GetCodeRefactoringProvider()
             => new ToPrismPropertyRefactoringProvider();
     }
