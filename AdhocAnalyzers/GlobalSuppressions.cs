a758fd275cc1a930141ac11b112547739823ec7f
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-09-12 21:40:19 +0200

    Suppress "Use [] instead of calling 'First'.",
    since I consider it premature optimization.

--- AdhocAnalyzers/GlobalSuppressions.cs
+++ AdhocAnalyzers/GlobalSuppressions.cs
@@ -1,4 +1,3 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("General", "RCS1118:Mark local variable as const.", Justification = "Otherwise 'var source' won't work.")]
 [assembly: SuppressMessage("Performance", "RCS1121:Use [] instead of calling 'First'.", Justification = "Premature optimization")]
