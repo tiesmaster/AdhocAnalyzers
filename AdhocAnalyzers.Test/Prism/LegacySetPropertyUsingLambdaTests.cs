1a6a88698622dc611cbf019275c5e568f63cccfb
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-09-12 23:11:34 +0200

    Implement first test.

--- AdhocAnalyzers.Test/Prism/LegacySetPropertyUsingLambdaTests.cs
+++ AdhocAnalyzers.Test/Prism/LegacySetPropertyUsingLambdaTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using AdhocAnalyzers.Prism;
 using AdhocAnalyzers.Test.Helpers;
-
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -23,6 +23,64 @@ namespace AdhocAnalyzers.Test.Prism
 
             VerifyDiagnostic(test);
         }
+
+        [Fact]
+        public void MyTestMethod()
+        {
+            var oldSource =
+@"class Class1
+{
+    private string _foo;
+
+    public string Foo
+    {
+        get
+        {
+            return _foo;
+        }
+        set
+        {
+            SetProperty(() => _foo = value, _foo, value);
+        }
+    }
+}";
+            var newSource =
+@"class Class1
+{
+    private string _foo;
+
+    public string Foo
+    {
+        get
+        {
+            return _foo;
+        }
+        set
+        {
+            SetProperty(ref _foo, value);
+        }
+    }
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "PRISM0001",
+                Message = "SetProperty() using lambda syntax is deprecated, use the default version instead.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 13)
+                        }
+            };
+
+            VerifyDiagnostic(oldSource, expected);
+            //VerifyFix(oldSource, newSource);
+        }
+
+        // TODO:
+        // no diagnostic: SetProperty(ref _foo, value);
+        // no diagnostic: SetProperty("Hoi", "dag")
+        // no diagnostic?: SetProperty in method, instead of setter
 
         protected override DiagnosticAnalyzer GetDiagnosticAnalyzer()
             => new LegacySetPropertyUsingLambdaAnalyzer();
--- AdhocAnalyzers/Prism/LegacySetPropertyUsingLambdaAnalyzer.cs
+++ AdhocAnalyzers/Prism/LegacySetPropertyUsingLambdaAnalyzer.cs
@@ -4,6 +4,9 @@ using System.Collections.Immutable;
 using System.Text;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace AdhocAnalyzers.Prism
@@ -23,7 +26,15 @@ namespace AdhocAnalyzers.Prism
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
 
         public override void Initialize(AnalysisContext context)
+            => context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
+
+        private void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
         {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            if (invocation.Expression is IdentifierNameSyntax name && name.Identifier.ValueText == "SetProperty")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(_rule, invocation.GetLocation()));
+            }
         }
     }
 }
