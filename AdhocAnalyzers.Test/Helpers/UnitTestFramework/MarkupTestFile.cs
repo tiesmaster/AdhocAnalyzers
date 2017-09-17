6ba37f585b55746722873f9b75e086d701f9853b
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-09-17 22:42:44 +0200

    Add copy information to source coming from Roslyn.

--- AdhocAnalyzers.Test/Helpers/UnitTestFramework/MarkupTestFile.cs
+++ AdhocAnalyzers.Test/Helpers/UnitTestFramework/MarkupTestFile.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+// Copied from Roslyn samples (dd8516b4b9:src/Samples/Shared/UnitTestFramework/MarkupTestFile.cs)
 
 using System;
 using System.Collections.Generic;
