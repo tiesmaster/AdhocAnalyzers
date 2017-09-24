985102c11f479f7bbb0de8a99f7695749df79ae0
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-09-24 21:10:41 +0200

    Extract IsMsTestMethod helper method.

--- AdhocAnalyzers/Xunit/MstestToXunitTRefactoringProvider.cs
+++ AdhocAnalyzers/Xunit/MstestToXunitTRefactoringProvider.cs
@@ -19,31 +19,29 @@ namespace AdhocAnalyzers.Xunit
             var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
             var currentNode = root.FindNode(context.Span);
 
-            if (currentNode is MethodDeclarationSyntax methodDeclaration)
+            if ((currentNode is MethodDeclarationSyntax methodDeclaration) && IsMsTestMethod(methodDeclaration))
             {
-                var isMsTestMethod = methodDeclaration
-                    .AttributeLists
-                    .Any(attributeList => attributeList
-                        .DescendantNodes()
-                        .OfType<IdentifierNameSyntax>()
-                        .Any(identifier => identifier.Identifier.ValueText == "TestMethod"));
+                context.RegisterRefactoring(
+                    CodeAction.Create("Convert MSTest method to Fact", _ =>
+                    {
+                        var testMethodToken = methodDeclaration
+                            .DescendantTokens()
+                            .Single(token => token.IsKind(SyntaxKind.IdentifierToken) && token.ValueText == "TestMethod");
 
-                if (isMsTestMethod)
-                {
-                    context.RegisterRefactoring(
-                        CodeAction.Create("Convert MSTest method to Fact", _ =>
-                        {
-                            var testMethodToken = methodDeclaration
-                                .DescendantTokens()
-                                .Single(token => token.IsKind(SyntaxKind.IdentifierToken) && token.ValueText == "TestMethod");
+                        var factToken = SyntaxFactory.Identifier("Fact");
 
-                            var factToken = SyntaxFactory.Identifier("Fact");
-
-                            var newRoot = root.ReplaceToken(testMethodToken, factToken);
-                            return Task.FromResult(context.Document.WithSyntaxRoot(newRoot));
-                        }));
-                }
+                        var newRoot = root.ReplaceToken(testMethodToken, factToken);
+                        return Task.FromResult(context.Document.WithSyntaxRoot(newRoot));
+                    }));
             }
+        }
+
+        private static bool IsMsTestMethod(MethodDeclarationSyntax methodDeclaration)
+        {
+            return methodDeclaration
+                .AttributeLists
+                .SelectMany(attr => attr.DescendantTokens())
+                .Any(token => token.IsKind(SyntaxKind.IdentifierToken) && token.ValueText == "TestMethod");
         }
     }
 }
