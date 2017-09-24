48cc9f530feb131c9c58e2a00c21cd0816415422
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-09-24 19:09:31 +0200

    Add skeleton for MStest -> xUnit refactoring.

---
+++ AdhocAnalyzers.Test/Xunit/MstestToXunitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdhocAnalyzers.Test.Helpers;
+using AdhocAnalyzers.Xunit;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Xunit;
+
+namespace AdhocAnalyzers.Test.Xunit
+{
+    public class MstestToXunitTests : CodeRefactoringVerifier
+    {
+        // TODO:
+        //  * Add action to convert TestMethod -> Fact (fix namespace import)
+        //    + Add support to also add ctor/dispose, that will call TI/TC
+        //  * Add action to remove last mstest TM (and remove the mstest namespace import)
+        //     + Add support to convert TI/TC into ctor/dispose, and cleanup added ctor, and dispose
+        //  * Add action to convert remaining test to xUnit, and
+        //      cleanup MStest -> xUnit bridge (remove TI/TC, and 
+
+        // CONVERT ALL
+        //  * Add action to convert all tests to xUnit
+        //    * convert TM -> Fact
+        //    * convert TI/TC -> ctor/dispose
+        //    * remove mstest namespace import
+
+        [Fact]
+        public void EmptySource_ShouldNotProvideRefactoring()
+        {
+            var source = "$$";
+            VerifyNoRefactoring(source);
+        }
+
+        protected override CodeRefactoringProvider GetCodeRefactoringProvider()
+        {
+            return new MstestToXunitTRefactoringProvider();
+        }
+    }
+}
---
+++ AdhocAnalyzers/Xunit/MstestToXunitTRefactoringProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+namespace AdhocAnalyzers.Xunit
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(StructureNamespaceUsingsRefactoringProvider))]
+    [Shared]
+    public class MstestToXunitTRefactoringProvider : CodeRefactoringProvider
+    {
+        public override Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
