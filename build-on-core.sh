608e74d24298e28dea930451e035de90b3d3ebf5
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-10-01 00:16:08 +0200

    Add couple of build scripts for .NET Core.

---
+++ build-on-core.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet build AdhocAnalyzers.Test/AdhocAnalyzers.Test.csproj -f netcoreapp2.0
---
+++ test-on-core.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet test AdhocAnalyzers.Test/AdhocAnalyzers.Test.csproj -f netcoreapp2.0
