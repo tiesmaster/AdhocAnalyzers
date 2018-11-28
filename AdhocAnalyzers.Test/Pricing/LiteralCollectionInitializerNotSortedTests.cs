3dc0e61e6a93bf0c2ffad95f227bfe96236b1b98
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-11-28 21:32:22 +0100

    wip

---
+++ AdhocAnalyzers.Test/Pricing/LiteralCollectionInitializerNotSortedTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AdhocAnalyzers.Pricing;
+using AdhocAnalyzers.Test.Helpers;
+
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace AdhocAnalyzers.Test.Pricing
+{
+    public class LiteralCollectionInitializerNotSortedTests : CodeFixVerifier
+    {
+        [Fact]
+        public void EmptySourceNoDiagnostics()
+        {
+            var test = "";
+
+            VerifyDiagnostic(test);
+        }
+
+        [Fact]
+        public void ClassWithoutCollectionInitializerNoDiagnostics()
+        {
+            var test =
+@"$$class Class1
+{
+}";
+            VerifyDiagnostic(test);
+        }
+
+        [Fact]
+        public void ClassWitSortedCollectionInitializerNoDiagnostics()
+        {
+            var test =
+@"$$class Class1
+{
+}";
+            VerifyDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetDiagnosticAnalyzer()
+            => new LiteralCollectionInitializerNotSortedAnalyzer();
+
+        protected override CodeFixProvider GetCodeFixProvider()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdhocAnalyzers/Pricing/LiteralCollectionInitializerNotSortedAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AdhocAnalyzers.Pricing
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class LiteralCollectionInitializerNotSortedAnalyzer : DiagnosticAnalyzer
+    {
+        private readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor(
+            "PRICING0001",
+            nameof(LiteralCollectionInitializerNotSortedAnalyzer),
+            "Given collection initializer has unsorted members.",
+            "PRICING",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Reports collection intializer without sorted members.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+        }
+    }
+}
